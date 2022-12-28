0addf315634f742a4bc6e86683087f93351825ee
tdubois <tdubois@student.42angouleme.fr>
2022-12-28 09:40:52 +0100

    v2
    reworked mandatory tester and wrote bonus tester

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!usr/bin/env bash
+
+TESTDIR=${TESTDIR:-tests}
+
+if [ -f "${TESTDIR}" ]; then
+	echo "ERROR: ${TESTDIR} already exists!";
+	exit 1;
+fi
+
+git clone --depth 1 git@github.com:thibautdbs/test_push_swap.git ${TESTDIR};
+
+chmod +x "${TESTDIR}/test_mandatory";
+chmod +x "${TESTDIR}/test_push_swap/test_bonus";
+echo "DONE";
